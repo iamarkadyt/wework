27de3625983614af988f4e8bc476135667595c17
iamarkadyt <pgp-noreply@rkd.dev>
2018-08-24 11:42:33 -0700

    Add random posts section to discover page.

--- client/src/components/DiscoverPage/DiscoverPage.js
+++ client/src/components/DiscoverPage/DiscoverPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './DiscoverPage.css'
 import mockProfileObject from '../../mocks/profile'
+import mockPosts from '../../mocks/posts'
 import ava_placeholder from '../../images/avatar_placeholder.png'
 import { Link } from 'react-router-dom'
 
@@ -8,19 +9,33 @@ const discoverPage = () => {
     let profiles = new Array(10).fill(mockProfileObject, 0, 10)
 
     return <div className="DiscoverPage-container">
-        <input type="text" placeholder="Search people by name, skill, posts..." className="search" />
+        <input type="text" placeholder="Search people or posts, by name, text, skills, etc..." className="search" />
         <div className="content">
-            {profiles.map(({ image, user, title, status, location, company, handle }, idx) => {
-                return <div className="node" key={`discover-node-${idx}`}>
-                    <img src={image || ava_placeholder} alt='' />
-                    <div className="text">
-                        <p>{user.name}</p>
-                        <p>{title} at {company}</p>
-                        <p>{status}, {location}</p>
+            <div className="people">
+                {profiles.map(({ image, user, title, status, location, company, handle }, idx) => {
+                    return <div className="node" key={`discover-node-profile-${idx}`}>
+                        <img src={image || ava_placeholder} alt='' />
+                        <div className="text">
+                            <p>{user.name}</p>
+                            <p>{title} at {company}</p>
+                            <p>{status}, {location}</p>
+                        </div>
+                        <Link to='/profile'><span>View Profile</span></Link>
                     </div>
-                    <Link to='/profile'><span>View Profile</span></Link>
-                </div>
-            })}
+                })}
+            </div>
+            <div className="posts">
+                {mockPosts.map(({ user, text, comments, likes }, idx) => {
+                    return <div className="node" key={`discover-node-post-${idx}`}>
+                        <p>{user.name} said:</p>
+                        <p>{text}</p>
+                        <p>
+                            <a href="#">{likes.length} [LIKE!]</a>
+                            <Link to='/comments'>{comments.length} comments</Link>
+                        </p>
+                    </div>
+                })}
+            </div>
         </div>
     </div>
 }
---
+++ client/src/mocks/posts.js
@@ -0,0 +1,53 @@
+export default new Array(10).fill(
+    {
+        "user": {
+            "_id": "some_alphanumeric_gibberish",
+            "name": "Dwayne Johnson",
+            "avatar": ""
+        },
+        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "comments": [
+            {
+                "user": {
+                    "_id": "some_alphanumeric_gibberish2",
+                    "name": "Vin Diesel",
+                    "avatar": ""
+                },
+                "text": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                "date": "1535133201"
+            },
+            {
+                "user": {
+                    "_id": "some_alphanumeric_gibberish2",
+                    "name": "Vin Diesel",
+                    "avatar": ""
+                },
+                "text": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                "date": "1535133201"
+            },
+            {
+                "user": {
+                    "_id": "some_alphanumeric_gibberish2",
+                    "name": "Vin Diesel",
+                    "avatar": ""
+                },
+                "text": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                "date": "1535133201"
+            }
+        ],
+        "likes": [
+            {
+                "_id": "19825918265102965",
+                "user": "aoeu9a8o10s392h0"
+            },
+            {
+                "_id": "19825918265102966",
+                "user": "aoeu9a8o10s392hx0"
+            },
+            {
+                "_id": "19825918265102967",
+                "user": "aoeu9a8o10s392120"
+            }
+        ],
+        "date": "1535133024"
+    }, 0, 10)
