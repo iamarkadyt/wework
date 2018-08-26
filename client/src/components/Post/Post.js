c14f71a81731c8fcd8738bdb0dd8c812c4c3420b
iamarkadyt <pgp-noreply@rkd.dev>
2018-08-25 17:16:43 -0700

    Sketch out Post component.

---
+++ client/src/components/Post/Post.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Post.css'
+
+const post = ({ user, text, comments, likes, date }) => {
+    return <div className="Post-container" key={`feed-node-${idx}`}>
+        <p className="header">
+            <span>{user.name} said:</span>
+            <span>{date}</span>
+        </p>
+        <p className="text">{text}</p>
+        <p className="buttons">
+            <span>{comments.length} comments</span>
+            <a href="#">{likes.length} likes</a>
+        </p>
+    </div>
+}
+
+export default post
