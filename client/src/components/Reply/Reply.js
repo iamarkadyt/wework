beb46dd8c5118b19d1a2b949a0f7f7aeec314b8c
iamarkadyt <pgp-noreply@rkd.dev>
2018-08-25 18:35:36 -0700

    Style Reply component, fix Comments page CSS.

--- client/src/components/Reply/Reply.js
+++ client/src/components/Reply/Reply.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { withFormik, Form } from 'formik'
+import './Reply.css'
 
 const reply = ({
     values,
     handleChange
 }) => {
-    return <Form>
+    return <Form className="Reply-container">
         <textarea rows="5" name="message" placeholder="Say something..."
             value={values.message} onChange={handleChange} />
         <button type="submit">Submit</button>
--- client/src/components/_Pages/Comments/Comments.js
+++ client/src/components/_Pages/Comments/Comments.js
@@ -3,12 +3,15 @@ import Post from '../../Post/Post'
 import Comment from '../../Comment/Comment'
 import Reply from '../../Reply/Reply'
 import { mockPost } from '../../../mocks/posts'
+import './Comments.css'
 
 const comments = () => {
     return <div className="Comments-container">
-        <Post {...mockPost} />
-        <Reply />
-        {mockPost.comments.map(item => <Comment {...item} />)}
+        <div className="content">
+            <Post {...mockPost} />
+            <Reply />
+            {mockPost.comments.map(item => <Comment {...item} />)}
+        </div>
     </div>
 }
 
