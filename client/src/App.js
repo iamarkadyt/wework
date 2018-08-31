b1f55145f354b1624c45847975a89576fdf281c2
iamarkadyt <pgp-noreply@rkd.dev>
2018-08-31 14:26:56 -0700

    Set up redux.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './state/store'
 import Layout from './components/Layout'
 import Navigation from './components/Navigation/Navigation'
 
 class App extends React.Component {
   render() {
-    return <Router>
-      <React.Fragment>
-        <Navigation />
-        <Layout />
-      </React.Fragment>
-    </Router>
+    return <Provider store={store}>
+      <Router>
+        <React.Fragment>
+          <Navigation />
+          <Layout />
+        </React.Fragment>
+      </Router>
+    </Provider>
   }
 }
 
