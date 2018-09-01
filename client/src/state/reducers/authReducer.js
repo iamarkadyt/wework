7e0706755f10fcae6dc9f03e09b1408bdf1b7b70
iamarkadyt <pgp-noreply@rkd.dev>
2018-08-31 23:16:29 -0700

    Store auth token in local storage, set axios default auth header.

--- client/src/state/reducers/authReducer.js
+++ client/src/state/reducers/authReducer.js
@@ -1,4 +1,6 @@
 import * as types from '../actions/types'
+import axios from 'axios'
+import jwtDecode from 'jwt-decode'
 
 const initialState = {
     isAuthenticated: false,
@@ -9,14 +11,16 @@ export default function (state = initialState, action) {
     switch (action.type) {
         case types.TEST_DISPATCH:
             return {}
-        case types.REGISTER_USER:
         case types.LOGIN_USER:
+            localStorage.setItem('jwt', action.payload.token)
+            axios.defaults.headers.common['Authorization'] = action.payload.token
             return {
-                ...state,
-                user: {
-                    token: action['payload']
-                }
+                isAuthenticated: true,
+                user: jwtDecode(action.payload.token)
             }
+        case types.LOGOUT_USER:
+            delete axios.defaults.headers.common['Authorization']
+            return initialState
         default:
             return state
     }
