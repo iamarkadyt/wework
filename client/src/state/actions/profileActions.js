bf7204b796121e109bf8fbdaec1f3b0a99dd0fc9
iamarkadyt <pgp-noreply@rkd.dev>
2018-09-04 15:28:33 -0700

    Add updateUsersProfile action.

--- client/src/state/actions/profileActions.js
+++ client/src/state/actions/profileActions.js
@@ -13,8 +13,21 @@ export const fetchUsersProfile = callback => dispatch => {
         })
         .catch(err => {
             dispatch({ type: types.POST_ERRORS, payload: err.response.data })
-            dispatch({ type: types.RESET_USERS_PROFILE })
+            // reset loading status:
+            dispatch({ type: types.FORGET_USERS_PROFILE })
         })
+}
+
+/**
+ * Create or update current user's profile
+ */
+export const updateUsersProfile = (data, callback) => dispatch => {
+    axios.post('/api/profile', data)
+        .then(res => {
+            dispatch({ type: types.POST_USERS_PROFILE, payload: res.data })
+            if (callback) callback()
+        })
+        .catch(err => dispatch({ type: types.POST_ERRORS, payload: err.response.data }))
 }
 
 // fetch_profiles
