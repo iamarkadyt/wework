c61e8d510a929feb0a86167c6b3acee799580df4
iamarkadyt <pgp-noreply@rkd.dev>
2018-09-04 21:39:39 -0700

    Reset app state completely on LOGOUT_USER

--- client/src/state/reducers/_root.js
+++ client/src/state/reducers/_root.js
@@ -1,12 +1,23 @@
+import * as types from '../actions/types'
 import { combineReducers } from 'redux'
 import authReducer from './authReducer'
 import errReducer from './errReducer'
 import profileReducer from './profileReducer'
 import overlayReducer from './overlayReducer'
 
-export default combineReducers({
+const appReducer = combineReducers({
     auth: authReducer,
     err: errReducer,
     profile: profileReducer,
     overlay: overlayReducer
 })
+
+const rootReducer = (state, action) => {
+    if (action.type === types.LOGOUT_USER) {
+        state = undefined
+    }
+
+    return appReducer(state, action)
+}
+
+export default rootReducer
