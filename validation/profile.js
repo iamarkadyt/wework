eb13d10c0ee3fbbcc1cf372142485c62f14a2e7c
iamarkadyt <pgp-noreply@rkd.dev>
2018-09-06 19:08:49 -0700

    Fix backend validation for education and experience forms data.

--- validation/profile.js
+++ validation/profile.js
@@ -29,37 +29,33 @@ module.exports = (profile, options = {
     let schema = {}
 
     if (options.onlyExperience) {
-        schema = Yup.array().of(Yup.object().shape({
+        schema = Yup.object().shape({
             title: Yup.string().required("Are you really trying to leave this out??"),
             company: Yup.string().required("Come on! It's not a shame to work for the Revature"),
             location: Yup.string(),
             from: Yup.string().required("I think you may be sleepy"),
-            to: Yup.string(),
-            current: Yup.bool().when('to', {
+            to: Yup.string().when('current', {
                 is: undefined,
-                then: Yup.bool().required("Do you work here presently?"),
-                other: Yup.bool().notRequired()
+                then: Yup.string().required("Do you work here presently?"),
+                otherwise: Yup.string().notRequired()
             }),
+            current: Yup.bool(),
             description: Yup.string().min(300, "Please enter at least 300 characters")
-        })).typeError("Please enter your experience" /* in case of empty json {} */)
-            .min(1, "Please include at least one position")
-            .required("Please include at least one position")
+        })
     } else if (options.onlyEducation) {
-        schema = Yup.array().of(Yup.object().shape({
+        schema = Yup.object().shape({
             school: Yup.string().required("Oh, please!"),
             degree: Yup.string().required("Come on!"),
             fieldOfStudy: Yup.string().required("Something unusual? Fill this out!"),
             from: Yup.string().required("Hey, fill this in!"),
-            to: Yup.string(),
-            current: Yup.bool().when('to', {
-                is: !undefined,
-                then: Yup.bool().required("Do you still study at your last school?"),
-                otherwise: Yup.bool().notRequired()
+            to: Yup.string().when('current', {
+                is: undefined,
+                then: Yup.string().required("Are you still studying at the last school specified?"),
+                otherwise: Yup.string().notRequired()
             }),
+            current: Yup.bool(),
             description: Yup.string().min(300, "Minimum of 300 characters is required!")
-        })).typeError("Please enter your education")
-            .min(1, "At least one position is required")
-            .required("We do not believe in self-taught devs!")
+        })
     } else { /* For whole profile with the exclusion of above described fields */
         schema = Yup.object().shape({
             handle: Yup.string()
