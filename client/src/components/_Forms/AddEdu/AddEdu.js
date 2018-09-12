a42b80f746aafe1e6075d8f3462763fbb5e17899
iamarkadyt <pgp-noreply@rkd.dev>
2018-09-11 18:51:40 -0700

    Replace old Overlay api w/ react-router's one at AddEdu.js

--- client/src/components/_Forms/AddEdu/AddEdu.js
+++ client/src/components/_Forms/AddEdu/AddEdu.js
@@ -1,17 +1,22 @@
 import React from 'react'
-import Field from '../../Field/Field'
 import { connect } from 'react-redux'
-import { addEducation } from '../../../state/actions/profileActions'
-import { dismissOverlay } from '../../../state/actions/overlayActions'
-import * as overlayTypes from '../../../helpers/overlayTypes'
 import 'react-widgets/dist/css/react-widgets.css'
 import './AddEdu.css'
+
+import Field from '../../Field/Field'
+import Overlay from '../../Overlay/Overlay'
+import { addEducation } from '../../../state/actions/profileActions'
 
 class AddEdu extends React.Component {
     state = {}
 
+    handleDismiss = () => {
+        const { history } = this.props
+        history.goBack()
+    }
+
     render() {
-        const { addEducation, dismissOverlay, errors } = this.props
+        const { addEducation, errors } = this.props
 
         const degreeTypes = [
             "Professional Certificate",
@@ -27,74 +32,77 @@ class AddEdu extends React.Component {
         ]
 
         return (
-            <form className="AddEdu-container" onSubmit={e => {
-                e.preventDefault()
-                addEducation(this.state, () => {
-                    dismissOverlay(overlayTypes.ADDING_EDUCATION)
-                })
-            }}>
-                <h1>Add Education</h1>
-                <Field type="text"
-                    name="school"
-                    placeholder="UCLA"
-                    value={this.state.school}
-                    onChange={e => this.setState({ school: e.target.value })}
-                    label="School:"
-                    error={errors.school} />
-                <Field type="text"
-                    name="fieldOfStudy"
-                    placeholder="Bioeconomics"
-                    value={this.state.fieldOfStudy}
-                    onChange={e => this.setState({ fieldOfStudy: e.target.value })}
-                    label="Field of Study:"
-                    error={errors.fieldOfStudy} />
-                <Field type="list"
-                    name="degree"
-                    value={this.state.degree}
-                    onChange={value => this.setState({ degree: value })}
-                    label="Degree:"
-                    list={degreeTypes}
-                    error={errors.degree} />
-                <Field type="date"
-                    name="from"
-                    value={this.state.from}
-                    onChange={value => this.setState({ from: value })}
-                    label="From:"
-                    error={errors.from} />
-                <Field
-                    type="date"
-                    name="to"
-                    value={this.state.to}
-                    onChange={value => this.setState({ to: value })}
-                    disabled={this.state.current}
-                    label="To:"
-                    error={errors.to} />
-                <Field
-                    type="checkbox"
-                    name="current"
-                    value={this.state.current}
-                    onChange={e => this.setState({ current: e.target.checked })}
-                    label="Current?" />
-                <Field
-                    type="textarea"
-                    name="description"
-                    value={this.state.description}
-                    onChange={e => this.setState({ description: e.target.value })}
-                    label="Description"
-                    rows="2"
-                    error={errors.description} />
-                <Field
-                    type="submit"
-                    label="Add" />
-                <Field
-                    type="button"
-                    label="Cancel"
-                    onClick={() => dismissOverlay(overlayTypes.ADDING_EDUCATION)} />
-            </form>
+            <Overlay onBackdropClick={() => this.handleDismiss()}>
+                <form className="AddEdu-container" onSubmit={e => {
+                    e.preventDefault()
+                    addEducation(this.state, () => this.handleDismiss())
+                }}>
+                    <h1>Add Education</h1>
+                    <Field type="text"
+                        name="school"
+                        placeholder="UCLA"
+                        value={this.state.school}
+                        onChange={e => this.setState({ school: e.target.value })}
+                        label="School:"
+                        error={errors.school} />
+                    <Field type="text"
+                        name="fieldOfStudy"
+                        placeholder="Bioeconomics"
+                        value={this.state.fieldOfStudy}
+                        onChange={e => this.setState({ fieldOfStudy: e.target.value })}
+                        label="Field of Study:"
+                        error={errors.fieldOfStudy} />
+                    <Field type="list"
+                        name="degree"
+                        value={this.state.degree}
+                        onChange={value => this.setState({ degree: value })}
+                        label="Degree:"
+                        list={degreeTypes}
+                        error={errors.degree} />
+                    <Field type="date"
+                        name="from"
+                        value={this.state.from}
+                        onChange={value => this.setState({ from: value })}
+                        label="From:"
+                        error={errors.from} />
+                    <Field
+                        type="date"
+                        name="to"
+                        value={this.state.to}
+                        onChange={value => this.setState({ to: value })}
+                        disabled={this.state.current}
+                        label="To:"
+                        error={errors.to} />
+                    <Field
+                        type="checkbox"
+                        name="current"
+                        value={this.state.current}
+                        onChange={e => this.setState({ current: e.target.checked })}
+                        label="Current?" />
+                    <Field
+                        type="textarea"
+                        name="description"
+                        value={this.state.description}
+                        onChange={e => this.setState({ description: e.target.value })}
+                        label="Description"
+                        rows="2"
+                        error={errors.description} />
+                    <Field
+                        type="submit"
+                        label="Add" />
+                    <Field
+                        type="button"
+                        label="Cancel"
+                        onClick={e => {
+                            e.preventDefault()
+                            this.handleDismiss()
+                        }} />
+                </form>
+            </Overlay>
         )
     }
 }
 
 export default connect(state => ({
     errors: state.err.formErrors
-}), { addEducation, dismissOverlay })(AddEdu)
+}), { addEducation })(AddEdu)
