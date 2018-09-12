051c920700deabe2f8de44098ab2d9fe5e4fc2b6
iamarkadyt <pgp-noreply@rkd.dev>
2018-09-11 18:46:20 -0700

    Delete overlay reducer, actions and types files.

--- client/src/state/actions/overlayActions.js
+++
@@ -1,9 +0,0 @@
-import * as types from './types'
-
-export const addOverlay = key => dispatch => {
-    dispatch({ type: types.ADD_OVERLAY, key })
-}
-
-export const dismissOverlay = key => dispatch => {
-    dispatch({ type: types.DISMISS_OVERLAY, key })
-}
--- client/src/state/reducers/_root.js
+++ client/src/state/reducers/_root.js
@@ -3,13 +3,11 @@ import { combineReducers } from 'redux'
 import authReducer from './authReducer'
 import errReducer from './errReducer'
 import profileReducer from './profileReducer'
-import overlayReducer from './overlayReducer'
 
 const appReducer = combineReducers({
     auth: authReducer,
     err: errReducer,
     profile: profileReducer,
-    overlay: overlayReducer
 })
 
 const rootReducer = (state, action) => {
