bc5cf642e4a245946dd4cab97ef64123e8cdae4f
iamarkadyt <pgp-noreply@rkd.dev>
2018-09-13 11:24:56 -0700

    Add new action types.

--- client/src/state/actions/types.js
+++ client/src/state/actions/types.js
@@ -9,6 +9,8 @@ export const POST_FORM_ERRORS = 'POST_FORM_ERRORS'
 // Profile actions
 export const POST_USERS_PROFILE = 'POST_USERS_PROFILE'
 
-// Overlay actions
-export const ADD_OVERLAY = 'ADD_OVERLAY'
-export const DISMISS_OVERLAY = 'DISMISS_OVERLAY'
+// Posts actions
+export const POST_NEW_POSTS = 'POST_NEW_POSTS'
+
+// Polling actions
+export const POLL_POSTS_POSITIVE = 'POLL_POSTS_POSITIVE'
