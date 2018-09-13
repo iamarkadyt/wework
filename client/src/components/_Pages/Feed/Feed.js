dfce3e50b178dba3d27a126ce194628073a4c263
iamarkadyt <pgp-noreply@rkd.dev>
2018-09-13 14:55:38 -0700

    Now fetch real posts at /feed

--- client/src/components/_Pages/Feed/Feed.js
+++ client/src/components/_Pages/Feed/Feed.js
@@ -1,20 +1,49 @@
 import React from 'react'
 import './Feed.css'
-import mockPosts from '../../../mocks/posts'
+import { connect } from 'react-redux'
+
 import Post from '../../Post/Post'
 import Reply from '../../Reply/Reply'
+import { fetchPosts } from '../../../state/actions/postsActions'
 
-const feed = () => {
-    const myCallback = (values) => {
-        console.log(values)
-    }
-
-    return <div className="Feed-container">
-        <Reply callback={myCallback} />
-        <div className="feed">
-            {mockPosts.map(item => <Post {...item} />)}
-        </div>
-    </div>
+const noContentPost = {
+    text: 'Subscribe to more people to make your feed more interesting!',
+    user: {
+        name: 'WeWork'
+    },
+    date: ''
 }
 
-export default feed
+// mainly 3/4ths of job is Java related, JavaScript goes for the frontend.
+// 60k remote?
+// should I look for more?
+
+const feed = ({
+    posts,
+    fetchPosts
+}) => {
+    let content = 'Please wait, loading...'
+
+    console.log('posts', posts)
+
+    if (!posts) {
+        fetchPosts()
+    } else if (posts) {
+        content = (
+            <div className="Feed-container">
+                <Reply />
+                <div className="feed">
+                    {Object.keys(posts).map(key => (
+                        <Post key={posts[key]['_id']} {...posts[key]} />)
+                    )}
+                </div>
+            </div>
+        )
+    }
+
+    return content
+}
+
+export default connect(state => ({
+    posts: state.posts
+}), { fetchPosts })(feed)
