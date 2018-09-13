24ed5ce46e654b31ca326b51a1a959ba6fdcd441
iamarkadyt <pgp-noreply@rkd.dev>
2018-09-13 11:24:06 -0700

    Restructure and restyle the Reply component.

--- client/src/components/Reply/Reply.js
+++ client/src/components/Reply/Reply.js
@@ -1,21 +1,43 @@
-import React from 'react'
-import { withFormik, Form } from 'formik'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addPost } from '../../state/actions/postsActions'
 import './Reply.css'
 
-const reply = ({
-    values,
-    handleChange
-}) => {
-    return <Form className="Reply-container">
-        <textarea rows="5" name="message" placeholder="Say something..."
-            value={values.message} onChange={handleChange} />
-        <button type="submit">Submit</button>
-    </Form>
+import Field from '../Field/Field'
+
+class Reply extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    clearFields = () => {
+        this.setState({ text: '' })
+    }
+
+    render() {
+        const { addPost, errors } = this.props
+
+        return <form className="Reply-container" onSubmit={e => {
+            e.preventDefault()
+            addPost(this.state, () => {
+                this.clearFields()
+            })
+        }}>
+            <Field
+                type="textarea"
+                rows="5"
+                value={this.state.text}
+                onChange={e => this.setState({ text: e.target.value })}
+                error={errors.text} />
+            <Field
+                type="submit"
+                inline
+                label="Submit" />
+        </form>
+    }
 }
 
-export default withFormik({
-    mapPropsToValues() { },
-    handleSubmit(values, { props }) {
-        props.callback(values)
-    }
-})(reply)
+export default connect(state => ({
+    errors: state.err.formErrors
+}), { addPost })(Reply)
