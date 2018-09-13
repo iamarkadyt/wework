d275c828c7c144a950f979c71512a79253c9b98b
iamarkadyt <pgp-noreply@rkd.dev>
2018-09-13 11:24:47 -0700

    Add addPost() action.

---
+++ client/src/state/actions/postsActions.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+import * as types from './types'
+
+export const addPost = (post, callback) => dispatch => {
+    axios.post('/api/posts', post)
+        .then(res => {
+            dispatch({ type: types.POST_NEW_POSTS, payload: res.data })
+            if (callback) callback()
+        })
+        .catch(err => {
+            dispatch({ type: types.POST_FORM_ERRORS, payload: err.response.data })
+        })
+}
+
