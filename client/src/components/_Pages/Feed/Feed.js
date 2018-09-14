b553d6652ec0a5907ee645a93c609c35d259d0db
iamarkadyt <pgp-noreply@rkd.dev>
2018-09-14 12:31:11 -0700

    Add /view-comments route to the Feed component.

--- client/src/components/_Pages/Feed/Feed.js
+++ client/src/components/_Pages/Feed/Feed.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './Feed.css'
 import { connect } from 'react-redux'
+import { Route } from 'react-router-dom'
 
 import Post from '../../Post/Post'
 import Reply from '../../Reply/Reply'
+import CommentsView from '../../CommentsView/CommentsView'
 import { fetchPosts, addPost } from '../../../state/actions/postsActions'
 
 const noContentPost = {
@@ -17,8 +19,10 @@ const noContentPost = {
 const feed = ({
     posts,
     fetchPosts,
-    addPost
+    addPost,
+    match
 }) => {
+    const baseUrl = match.url || ''
     let content = 'Please wait, loading...'
 
     if (posts.length === 0) {
@@ -32,6 +36,11 @@ const feed = ({
                         <Post key={item._id} {...item} />)
                     )}
                 </div>
+                <Route path={`${baseUrl}/view-comments/:postId`} render={props => (
+                    <CommentsView {...props} post={posts.find(
+                        item => item._id === props.match.params.postId
+                    )} />
+                )} />
             </div>
         )
     }
