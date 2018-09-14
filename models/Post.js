3eb2102f98509fc39cb2ab862564e5a93e36f147
iamarkadyt <pgp-noreply@rkd.dev>
2018-09-13 18:41:25 -0700

    Remove name and avatar fields from Post schema.

--- models/Post.js
+++ models/Post.js
@@ -4,8 +4,6 @@ const Schema = mongoose.Schema
 const PostSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'users' },
     text: { type: String, required: true },
-    name: { type: String },
-    avatar: { type: String },
     likes: [{ user: { type: Schema.Types.ObjectId, ref: 'users' } }],
     comments: [{
         user: { type: Schema.Types.ObjectId, ref: 'users' },
