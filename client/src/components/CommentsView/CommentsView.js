405d2472d5dbea086c6c4db71fbbd6ec8b59674c
iamarkadyt <pgp-noreply@rkd.dev>
2018-09-14 21:53:47 -0700

    Fix the 'data undefined' error when deleting a Post from CommentsView.

--- client/src/components/CommentsView/CommentsView.js
+++ client/src/components/CommentsView/CommentsView.js
@@ -30,38 +30,50 @@ const CommentNode = ({
         </div>
     )
 
-const commentsView = ({
-    post,
-    post: {
-        _id,
-        comments,
-    },
-    history,
-    addComment
-}) => {
-    const handleDismiss = () => {
+class CommentsView extends Component {
+    handleDismiss = () => {
+        const { history } = this.props
         history.goBack()
     }
 
-    return (
-        <Overlay onBackdropClick={() => handleDismiss()}>
-            <div className="CommentsView-container">
-                <div className="CommentsView-content">
-                    <Post {...post} flat nocomments />
-                    <Reply
-                        rows="3"
-                        flat
-                        onSubmit={(data, callback) => addComment(_id, data, callback)} />
-                    <div className="CommentsView-comments">
-                        <h2>Comments:</h2><br />
-                        {comments.length > 0
-                            ? comments.map(item => <CommentNode {...item} />)
-                            : <p>Be the first one to leave a comment!</p>}
+    shouldComponentUpdate(nextProps) {
+        if (!nextProps.post) {
+            this.handleDismiss()
+            return false
+        }
+        return true
+    }
+
+    render() {
+        const {
+            post,
+            post: {
+                _id,
+                comments
+            },
+            addComment
+        } = this.props
+
+        return (
+            <Overlay onBackdropClick={() => this.handleDismiss()}>
+                <div className="CommentsView-container">
+                    <div className="CommentsView-content">
+                        <Post {...post} flat nocomments />
+                        <Reply
+                            rows="3"
+                            flat
+                            onSubmit={(data, callback) => addComment(_id, data, callback)} />
+                        <div className="CommentsView-comments">
+                            <h2>Comments:</h2><br />
+                            {comments.length > 0
+                                ? comments.map(item => <CommentNode {...item} />)
+                                : <p>Be the first one to leave a comment!</p>}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </Overlay>
-    )
+            </Overlay>
+        )
+    }
 }
 
-export default connect(null, { addComment })(commentsView)
+export default connect(null, { addComment })(CommentsView)
