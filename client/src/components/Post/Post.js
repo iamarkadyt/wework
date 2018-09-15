a08db83eae5ec1e15726360166cfacdd09d0e89a
iamarkadyt <pgp-noreply@rkd.dev>
2018-09-14 19:07:58 -0700

    Add dropdown menu to Post.js, flatten comp.'s SCSS class hierarchy.

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Post.css'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
@@ -12,90 +12,120 @@ import Field from '../Field/Field'
 import placeholderImage from '../../images/avatar_placeholder.png'
 import { likePost, deleteLike } from '../../state/actions/postsActions'
 
-const post = ({
-    _id,
-    user: {
-        name,
-        avatar
-    },
-    text,
-    comments,
-    likes,
-    date,
-    authedUser,
-    likePost,
-    deleteLike,
-    history,
-    match,
-    flat,
-    nocomments
-}) => {
-    const likedByAuthedUser = !!likes.find(item => item.user === authedUser.id)
-    const baseUrl = match.url || ''
-    const dateFormatOptions = {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric'
-    }
-    const flatStyle = {
-        borderRadius: 'unset',
-        boxShadow: 'unset',
-        marginTop: 'unset',
+class Post extends Component {
+    state = {
+        showMenu: false
     }
 
-    /**
-     * Ava, Name
-     * Title
-     * When
-     * 
-     * Body
-     * Media
-     * 
-     * Stats
-     * Buttons
-     */
+    componentWillMount() {
+        window.addEventListener('click', e => {
+            if (e.target.className !== "Post__button--menu")
+                this.setState({ showMenu: false })
+        })
+    }
 
-    return <div className="Post__container" style={flat ? flatStyle : null}>
-        <div className="Post__header">
-            <img className="Post__avatar" src={avatar || placeholderImage} alt='' />
-            <button className="Post__button--more"><IcoMore /></button>
-            <p className="Post__name">{name}</p>
-            <p className="Post__date">{new Date(date).toLocaleDateString('en-US', dateFormatOptions)}</p>
-        </div>
-        <p className="Post__body">{text}</p>
-        <div className="Post__stats">
-            <span>{likes.length} likes &nbsp;•&nbsp; {comments.length} comments</span>
-        </div>
-        <div className="Post__buttons">
-            <Field
-                type="linkButton"
-                inline
-                style={{ color: likedByAuthedUser ? null : 'gray' }}
-                onClick={() => {
-                    likedByAuthedUser
-                        ? deleteLike(_id)
-                        : likePost(_id)
-                }}>
-                <span className="Post__buttons-icon"><IcoLike /></span>
-                &nbsp;Like
-            </Field>
-            {nocomments
-                ? null
-                : <Field
+    render() {
+        const {
+            _id,
+            user: {
+                name,
+                avatar
+            },
+            text,
+            comments,
+            likes,
+            date,
+            authedUser,
+            likePost,
+            deleteLike,
+            history,
+            match,
+            flat,
+            nocomments
+        } = this.props
+        const likedByAuthedUser = !!likes.find(item => item.user === authedUser.id)
+        const baseUrl = match.url || ''
+        const dateFormatOptions = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric'
+        }
+        const flatStyle = {
+            borderRadius: 'unset',
+            boxShadow: 'unset',
+            marginTop: 'unset',
+        }
+
+        return <div className="Post__container" style={flat ? flatStyle : null}>
+            <div className={[
+                "Post__menu",
+                this.state.showMenu
+                    ? "Post__menu--shown"
+                    : ""
+            ].join(' ')}>
+                <button
+                    className="Post__menu-item"
+                    onClick={() => alert('deleted')}>
+                    Delete
+            </button>
+                <button
+                    className="Post__menu-item"
+                    onClick={() => alert('deleted')}>
+                    Unfollow user
+            </button>
+                <button
+                    className="Post__menu-item"
+                    onClick={() => alert('deleted')}>
+                    Report
+            </button>
+            </div>
+            <div className="Post__header">
+                <img className="Post__avatar" src={avatar || placeholderImage} alt='' />
+                <button
+                    className="Post__button--menu"
+                    onClick={() => this.setState(
+                        prevState => ({ showMenu: !prevState.showMenu })
+                    )}>
+                    <IcoMore />
+                </button>
+                <p className="Post__name">{name}</p>
+                <p className="Post__date">{new Date(date).toLocaleDateString('en-US', dateFormatOptions)}</p>
+            </div>
+            <p className="Post__body">{text}</p>
+            <div className="Post__stats">
+                <span>{likes.length} likes &nbsp;•&nbsp; {comments.length} comments</span>
+            </div>
+            <div className="Post__buttons">
+                <Field
                     type="linkButton"
                     inline
-                    style={{ color: 'gray', marginLeft: '1rem' }}
-                    onClick={() => history.push(`${baseUrl}/view-comments/${_id}`)}>
-                    <span className="Post__buttons-icon"><IcoComments /></span>
-                    &nbsp;Comment
+                    style={{ color: likedByAuthedUser ? null : 'gray' }}
+                    onClick={() => {
+                        likedByAuthedUser
+                            ? deleteLike(_id)
+                            : likePost(_id)
+                    }}>
+                    <span className="Post__buttons-icon"><IcoLike /></span>
+                    &nbsp;Like
+            </Field>
+                {nocomments
+                    ? null
+                    : <Field
+                        type="linkButton"
+                        inline
+                        style={{ color: 'gray', marginLeft: '1rem' }}
+                        onClick={() => history.push(`${baseUrl}/view-comments/${_id}`)}>
+                        <span className="Post__buttons-icon"><IcoComments /></span>
+                        &nbsp;Comment
             </Field>}
+            </div>
         </div>
-    </div>
+    }
 }
 
 export default withRouter(connect(state => ({
     authedUser: state.auth.user
-}), { likePost, deleteLike })(post))
+}), { likePost, deleteLike })(Post))
