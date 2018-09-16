b76a7ea0ce580b54bf2c8ec64251ac1a35aa928e
iamarkadyt <pgp-noreply@rkd.dev>
2018-09-15 19:13:10 -0700

    Listen to post_subscriptions &post_followers action types at userReducer

--- client/src/state/reducers/userReducer.js
+++ client/src/state/reducers/userReducer.js
@@ -1,6 +1,5 @@
 import * as types from '../actions/types'
 import axios from 'axios'
-import jwtDecode from 'jwt-decode'
 
 const initialState = {
     isAuthenticated: false
@@ -13,12 +12,22 @@ export default function (state = initialState, action) {
             axios.defaults.headers.common['Authorization'] = action.payload.token
             return {
                 isAuthenticated: true,
-                ...jwtDecode(action.payload.token)
+                ...action.payload.decodedToken
             }
         case types.LOGOUT_USER:
             localStorage.removeItem('jwt')
             delete axios.defaults.headers.common['Authorization']
             return initialState
+        case types.POST_USERS_SUBSCRIPTIONS:
+            return {
+                ...state,
+                following: action.payload
+            }
+        case types.POST_USERS_FOLLOWERS:
+            return {
+                ...state,
+                followers: action.payload
+            }
         default:
             return state
     }
