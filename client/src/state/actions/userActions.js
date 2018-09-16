3003d7c70d2d2a6f1f5a4dcc241cb5a45caf31f4
iamarkadyt <pgp-noreply@rkd.dev>
2018-09-15 19:10:41 -0700

    Decode jwt token in userActions.js

--- client/src/state/actions/userActions.js
+++ client/src/state/actions/userActions.js
@@ -1,27 +1,28 @@
 import * as types from './types'
 import axios from 'axios'
+import jwtDecode from 'jwt-decode'
 
 export const loginUser = (credentials, callback) => dispatch => {
     axios.post('/api/users/login', credentials)
         .then(res => {
-            /* Small note: reducer function is called within the dispatch.
-             * Rest assured that by the end of the dispatch() method execution
-             * state is going to be updated.
+            /* Reducer function is executed within the dispatch, so that
+             * by the end of the dispatch() state is updated 
+             * and subscribers are notified.
              * https://github.com/reduxjs/redux/blob/56c94433703d327bd5b023d67ab50fd678b775ce/src/createStore.js#L186
              */
+            const decodedToken = jwtDecode(res.data.token)
             dispatch({
                 type: types.LOGIN_USER,
-                payload: { token: res.data.token }
+                payload: { 
+                    token: res.data.token,
+                    decodedToken
+                }
             })
-            /* So redirecting user to the protected route in the callback would work, 
-             * as the reducer would be already finished executing and isAuthenticated 
-             * flag would be set to true.
-             */ 
-            if (callback) callback()
+            if (callback) callback(decodedToken.id)
         })
         .catch(err => dispatch({
             type:
-                types.POST_ERRORS, payload:
+                types.POST_FORM_ERRORS, payload:
                 err.response.data
         }))
 }
@@ -34,14 +35,18 @@ export const logoutUser = callback => dispatch => {
 export const registerUser = (formData, callback) => dispatch => {
     axios.post('/api/users/register', formData)
         .then(res => {
+            const decodedToken = jwtDecode(res.data.token)
             dispatch({
                 type: types.LOGIN_USER,
-                payload: { token: res.data.token }
+                payload: { 
+                    token: res.data.token,
+                    decodedToken
+                }
             })
             if (callback) callback()
         })
         .catch(err => dispatch({
-            type: types.POST_ERRORS,
+            type: types.POST_FORM_ERRORS,
             payload: err.response.data
         }))
 }
