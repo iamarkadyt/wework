130b87b67bb4798d6109a5aabf70f2496c011c21
iamarkadyt <pgp-noreply@rkd.dev>
2018-09-16 09:53:22 -0700

    Connect viewedProfileReducer

--- client/src/state/reducers/_root.js
+++ client/src/state/reducers/_root.js
@@ -4,12 +4,14 @@ import userReducer from './userReducer'
 import errReducer from './errReducer'
 import profileReducer from './profileReducer'
 import postsReducer from './postsReducer'
+import viewedProfileReducer from './viewedProfileReducer'
 
 const appReducer = combineReducers({
     user: userReducer,
     err: errReducer,
     profile: profileReducer,
-    posts: postsReducer
+    posts: postsReducer,
+    viewedProfile: viewedProfileReducer
 })
 
 const rootReducer = (state, action) => {
