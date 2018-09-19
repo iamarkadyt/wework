a69faf5cf74ea0bf0caf9eeab94e90c2bdd86891
iamarkadyt <pgp-noreply@rkd.dev>
2018-09-18 18:13:46 -0700

    Update postsReducer to support reset flag

--- client/src/state/reducers/postsReducer.js
+++ client/src/state/reducers/postsReducer.js
@@ -5,7 +5,11 @@ const initialState = []
 export default function (state = initialState, action) {
     switch (action.type) {
         case types.POST_NEW_POSTS:
-            return [...state, ...action.payload]
+            if (action.withReset) {
+                return action.payload
+            } else {
+                return [...state, ...action.payload]
+            }
         case types.UPDATE_POST:
             return state.map(item => {
                 return item._id === action.payload._id
