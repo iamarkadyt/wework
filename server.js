75e364f7833ce46fa93af3f49cfa132a19bf483f
iamarkadyt <pgp-noreply@rkd.dev>
2018-09-22 22:04:41 -0700

    Add heroku-postbuild script

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
+const path = require('path')
 const bodyParser = require('body-parser')
 const passport = require('passport')
 const postsRouter = require('./routes/api/posts')
@@ -15,6 +17,12 @@ app.use('/api/posts', postsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/profile', profileRouter)
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 
 const mongoose = require('mongoose')
