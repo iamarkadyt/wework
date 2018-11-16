5f986d7a58a77917696ac6e9d8c1df3de3bdfa85
iamarkadyt <pgp-noreply@rkd.dev>
2018-11-15 22:24:22 -0800

    Temporarily disable dev tools support.

--- client/src/state/store.js
+++ client/src/state/store.js
@@ -4,7 +4,6 @@ import rootReducer from './reducers/_root'
 
 const middleware = [thunk]
 const initialState = {}
-const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware)))
 
 export default store
