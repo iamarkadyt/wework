3fc04f765c68345c35a159a8919c97005b52e599
iamarkadyt <pgp-noreply@rkd.dev>
2018-11-15 22:45:20 -0800

    Add a message about future smaller screens support.

--- client/src/components/Greeter/Greeter.js
+++ client/src/components/Greeter/Greeter.js
@@ -28,6 +28,11 @@ class Greeter extends React.Component {
             startAnimations && "animate"
         ].join(' ')
 
+        const headlineWarningClasses = [
+            "Greeter__headline_warning",
+            startAnimations && "animate"
+        ].join(' ')
+
         return (
             <div className="Greeter__container">
                 <div className={logoScreenClasses}>
@@ -36,6 +41,16 @@ class Greeter extends React.Component {
                     </div>
                     <span className={headlineClasses}>
                         Social network website for professionals
+                    </span>
+                    <span className={headlineWarningClasses}>
+                        Support for the mobiles is on it's way!<br />
+                        In the meanwhile checkout&nbsp;
+                        <a href="https://arkadyt.com" target="_blank">
+                            <b>arkadyt.com</b>
+                        </a>
+                        <br />
+                        where I demonstrate my skills in building<br />
+                        the responsive layouts!
                     </span>
                 </div>
                 <div className={whiteScreenClasses} />
