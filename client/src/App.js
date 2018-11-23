58c2ea3a361b17470cd05bedf08796b826175b57
iamarkadyt <pgp-noreply@rkd.dev>
2018-11-23 13:47:08 -0800

    Upgrade major dependencies.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './state/store'
 import Layout from './components/Layout'
 import Navigation from './components/Navigation/Navigation'
 import Greeter from './components/Greeter/Greeter'
+import './App.scss'
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <div className="App-container">
@@ -20,8 +21,8 @@ class App extends React.Component {
         </Provider>
         <Greeter />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
