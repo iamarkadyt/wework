a288b938cb22c50ac8569cc513a8e36631179596
iamarkadyt <pgp-noreply@rkd.dev>
2018-12-12 12:47:03 -0800

    Hide redux devtools in production

--- client/src/state/store.js
+++ client/src/state/store.js
@@ -2,8 +2,22 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/_root'
 
+let store
 const middleware = [thunk]
 const initialState = {}
-const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware)))
+
+if (process.env.NODE_ENV === 'development') {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  store = createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(...middleware)))
+} else {
+  store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware)))
+}
 
 export default store
+
+
+
+
+
+
+
