63b2e32e1e4148d0700f2ef93086da871bf07cae
iamarkadyt <pgp-noreply@rkd.dev>
2018-12-18 20:16:19 -0800

    Ditch static file serving with express.

--- server.js
+++ server.js
@@ -10,20 +10,11 @@ const profileRouter = require('./routes/api/profile')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(passport.initialize())
-
 require('./config/passport')(passport)
 
 app.use('/api/posts', postsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/profile', profileRouter)
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
-
 
 const mongoose = require('mongoose')
 const db = require('./config/keys').mongoURI
@@ -32,7 +23,5 @@ mongoose
     .then(() => console.log('Connected to database.'))
     .catch((err) => console.log('Could not connect to database.\n' + err))
 
-
-
-const port = process.env.PORT || 5000
-app.listen(port, () => console.log('Application listening on port: ' + port))
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log('Application listening on port: ' + PORT))
