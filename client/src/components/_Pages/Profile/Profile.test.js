2ce479b9655a0092f9581f230e9cbcc428f468c9
iamarkadyt <pgp-noreply@rkd.dev>
2019-01-14 23:27:25 -0800

    Complete Profile component test suite

--- client/src/components/_Pages/Profile/Profile.test.js
+++ client/src/components/_Pages/Profile/Profile.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Profile } from './Profile'
+import { Profile, mapStateToProps } from './Profile'
 import { shallow } from 'enzyme'
 import { BrowserRouter } from 'react-router-dom'
 import cloneDeep from 'lodash.clonedeep'
@@ -17,7 +17,7 @@ const getMockProps = () => {
       url: '/profile'
     },
     fetchUsersProfile: jest.fn(),
-    deleteAProfile: jest.fn(),
+    deleteProfile: jest.fn(cb => cb()),
     fetchAProfile: jest.fn(),
     viewedProfile: mockProfile,
     profile: mockProfile,
@@ -107,11 +107,139 @@ describe('Profile', () => {
         beforeEach(() => {
           props.viewedProfile = mockProfile
         })
+
+        it('the only one and always rendered route', () => {
+          const id = props.viewedProfile.user._id
+          props.match.url = `/profile/id/${id}`
+          props.match.params = { userId: id }
+          const context = createRouterContext(`${props.match.url}`)
+          expect(comp().find(`Route`).dive({ context })).toMatchSnapshot()
+        })
       })
     })
 
     describe('conditional rendering outputs', () => {
+      describe('when match.url includes /id/', () => {
+        beforeEach(() => {
+          props.match.url = '/profile/id/something'
+        })
 
+        it('renders message that viewed user has no profile', () => {
+          props.errors.noViewedProfile = 'error!'
+          expect(comp()).toMatchSnapshot()
+        })
+
+        it('renders viewed users profile', () => {
+          delete props.errors.noViewedProfile
+          props.viewedProfile = mockProfile
+
+          const id = props.viewedProfile.user._id
+          props.match.url = `/profile/id/${id}`
+          props.match.params = { userId: id }
+
+          expect(comp()).toMatchSnapshot()
+        })
+
+        it('otherwise calls fetchAProfile', () => {
+          delete props.errors.noViewedProfile
+          delete props.viewedProfile
+
+          const id = 'id of not yet loaded user'
+          props.match.url = `/profile/id/${id}`
+          props.match.params = { userId: id }
+
+          comp()
+          expect(props.fetchAProfile).toHaveBeenCalledWith(id)
+        })
+      })
+
+      describe('when match.url does not contain /id/', () => {
+        beforeEach(() => {
+          props.match.url = '/profile'
+        })
+
+        it('renders two routes if props.errors.noProfile is defined', () => {
+          props.errors.noProfile = 'defined!'
+          delete props.profile
+          expect(comp()).toMatchSnapshot()
+        })
+
+        it('renders five routes if props.profile is defined', () => {
+          props.profile = mockProfile
+          delete props.errors.noProfile
+          expect(comp()).toMatchSnapshot()
+        })
+
+        it('renders five routes if props.profile is defined', () => {
+          delete props.profile
+          delete props.errors.noProfile
+
+          comp()
+          expect(props.fetchUsersProfile).toHaveBeenCalled()
+        })
+      })
+    })
+  })
+
+  describe('props', () => {
+    describe('passing', () => {
+      describe('create profile modal', () => {
+        beforeEach(() => {
+          props.match.url = '/profile'
+          props.errors.noProfile = 'defined'
+        })
+
+        it('receives correct onConfirm function', () => {
+          const context = createRouterContext(`${props.match.url}`)
+          comp().find(`Route[path="${props.match.url}"]`).dive({ context }).simulate('confirm')
+          expect(props.history.push).toHaveBeenCalledWith(`${props.match.url}/create-profile`)
+        })
+
+        it('receives correct onDismiss function', () => {
+          const context = createRouterContext(`${props.match.url}`)
+          comp().find(`Route[path="${props.match.url}"]`).dive({ context }).simulate('dismiss')
+          expect(props.history.push).toHaveBeenCalledWith(`/feed`)
+        })
+      })
+
+      describe('delete profile modal', () => {
+        beforeEach(() => {
+          props.match.url = '/profile'
+          delete props.errors.noProfile
+          props.profile = mockProfile
+        })
+
+        it('receives correct onConfirm function', () => {
+          const context = createRouterContext(`${props.match.url}/delete-profile`)
+          comp().find(`Route[path*="/delete-profile"]`).dive({ context }).simulate('confirm')
+          expect(props.deleteProfile).toHaveBeenCalledWith(expect.any(Function))
+          expect(props.history.push).toHaveBeenCalledWith(props.match.url)
+        })
+
+        it('receives correct onDismiss function', () => {
+          const context = createRouterContext(`${props.match.url}/delete-profile`)
+          comp().find(`Route[path*="/delete-profile"]`).dive({ context }).simulate('dismiss')
+          expect(props.history.goBack).toHaveBeenCalled()
+        })
+      })
+    })
+  })
+
+  describe('mapStateToProps', () => {
+    it('returns expected object', () => {
+      const state = {
+        profile: { a: 1, b: 900 },
+        err: {},
+        viewedProfile: mockProfile
+      }
+
+      const expectedObj = {
+        profile: state.profile,
+        errors: state.err,
+        viewedProfile: state.viewedProfile
+      }
+
+      expect(mapStateToProps(state)).toStrictEqual(expectedObj)
     })
   })
 })
