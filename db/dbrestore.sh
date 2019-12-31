e0b76e896a7d90eb7e5121082b7d637fbda57a9f
iamarkadyt <pgp-noreply@rkd.dev>
2019-12-31 15:58:39 -0800

    Disabl db auth since it's not reachable from internet

--- db/dbrestore.sh
+++ db/dbrestore.sh
@@ -2,19 +2,12 @@
 
 DIRPATH=$(pwd)
 
-# load credentials
-source $DIRPATH/../.env
-export $MONGO_INITDB_ROOT_USERNAME $MONGO_INITDB_ROOT_PASSWORD
-
 # restore db
 echo ::: restoring wework database from backup... :::
 mongorestore \
   --drop \
   --preserveUUID \
   --restoreDbUsersAndRoles \
-  --authenticationDatabase=admin \
   --db=wework \
   --host=localhost:27017 \
-  --username=$MONGO_INITDB_ROOT_USERNAME \
-  --password=$MONGO_INITDB_ROOT_PASSWORD \
   $DIRPATH/wework
