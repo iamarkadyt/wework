55f525b7b3eafa5d79afb14b84d5add9016fa7d7
iamarkadyt <pgp-noreply@rkd.dev>
2020-04-22 21:54:38 -0700

    Add custom deploy script

---
+++ client/deploy.sh
@@ -0,0 +1,18 @@
+#
+# https://github.com/apancutt/deploy-aws-s3-cloudfront
+#
+
+# import aws key variables from .env file
+export $(xargs < ./.env)
+
+deploy-aws-s3-cloudfront \
+  --acl private \
+  --bucket apphost-7 \
+  --cache-control index.html:no-cache \
+  --delete \
+  --source ./build \
+  --destination wework \
+  --distribution E2IF8ARGIUFEIM \
+  --invalidation-path "/*" \
+  --non-interactive
+
