f94c7a5e4a5df7822f1de9d6103c98189452bc44
iamarkadyt <pgp-noreply@rkd.dev>
2020-05-16 11:41:59 -0700

    Update CORS url

--- server/config/cors.policy.js
+++ server/config/cors.policy.js
@@ -5,7 +5,7 @@ let whitelist
 if (process.env.NODE_ENV === 'development') {
   whitelist = ['http://localhost:3000']
 } else {
-  whitelist = ['https://wework.arkadyt.com']
+  whitelist = ['https://wework.arkadyt.dev']
 }
 
 const corsOptions = {
